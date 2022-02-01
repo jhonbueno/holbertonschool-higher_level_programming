26755cab969f1dcbc9ad4814f6f4832212f7c5e8
Jhon Bueno <jhon@monbu.co>
2022-01-31 19:58:45 -0500

    Update 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""function that returns True if the object is an instance of a class """
+
+
 def inherits_from(obj, a_class):
+    """Return if is a subclass"""
     return issubclass(type(obj), a_class)
