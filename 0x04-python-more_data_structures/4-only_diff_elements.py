8b574a88110916f63545c01825fdc60b9b74e3fd
Jhon Bueno <jhon@monbu.co>
2022-02-02 09:57:21 -0500

    Set unique

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    set_unique = set_1.symetric_difference(set_2)
-    return set_unique
+    return set_1.symetric_difference_update(set_2)
