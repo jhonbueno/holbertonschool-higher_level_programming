df4a86fb348269878432d3801ac1b4dc503c19d3
Jhon Bueno <jhon@monbu.co>
2022-02-02 09:14:54 -0500

    Unique

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     new_list = list(dict.fromkeys(my_list))
-    return new_list
+    sum = 0
+    for i in new_list:
+        sum += i
+    return sum
