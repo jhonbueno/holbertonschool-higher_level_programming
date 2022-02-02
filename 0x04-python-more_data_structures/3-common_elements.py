79443b7c6b80672daf02e3330ebbe4c03a6d198d
Jhon Bueno <jhon@monbu.co>
2022-02-02 09:29:08 -0500

    Common elements

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    set_common = {}
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                set_common.add(i)
+                break
+    return set_common
