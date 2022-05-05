50c2a164aec311cfa49f1cf02d48f4387ab2bd65
Jhon Bueno <jhon@monbu.co>
2022-05-05 17:53:57 +0000

    Update Calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -16,16 +16,12 @@ if __name__ == "__main__":
 
     if operator == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
-        exit(0)
     elif operator == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
-        exit(0)
     elif operator == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
-        exit(0)
     elif operator == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
-        exit(0)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
