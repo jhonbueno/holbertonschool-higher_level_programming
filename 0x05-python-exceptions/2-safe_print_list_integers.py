9d92adc9fa8106e7758702d35912a1fa685fd4c3
Jhon Bueno <jhon@monbu.co>
2022-05-25 13:49:17 +0000

    Update

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,5 @@ def safe_print_list_integers(my_list=[], x=0):
             pass
         except TypeError:
             pass
-        except IndexError:
-            pass
     print("")
     return(count)
