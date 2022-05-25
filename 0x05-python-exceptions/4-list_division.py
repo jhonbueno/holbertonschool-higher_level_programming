cb921eecfc6330c6ed08f299dd6dddefac1b58dc
Jhon Bueno <jhon@monbu.co>
2022-05-25 14:21:35 +0000

    Update list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(0, list_length):
         try:
-            result = my_list[i] / my_list_2
+            result = my_list[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
             result = 0
@@ -12,6 +12,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         except IndexError:
             print("out of range")
+            result = 0
         finally:
             new_list.append(result)
     return(new_list)
