6d8fc18e4037555a82f8ff9175e791cfc50adafc
Jhon Bueno <jhon@monbu.co>
2022-05-25 14:00:31 +0000

    3 safe division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        resut = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+
+    return(count)
