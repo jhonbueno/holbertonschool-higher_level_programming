2a118b0fab1191e787ac7e6cd688813de4af72ad
Jhon Bueno <jhon@monbu.co>
2022-05-28 00:58:40 +0000

    Setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -16,6 +16,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     @property
     def size(self):
         return self.__size
@@ -28,7 +29,6 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-
 
     def area(self):
         return self.__size ** 2
